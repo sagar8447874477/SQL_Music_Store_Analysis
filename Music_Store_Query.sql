83e5bc3a7edd211112f1a7cd10410cf4d8b9033f
Sagar Dabas <43811742+sagar8447874477@users.noreply.github.com>
2023-04-02 16:56:29 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -1,5 +1,3 @@
-/*	Question Set 1 - Easy */
-
 /* Q1: Who is the senior most employee based on job title? */
 
 SELECT title, last_name, first_name 
@@ -46,8 +44,6 @@ LIMIT 1;
 
 
 
-
-/* Question Set 2 - Moderate */
 
 /* Q1: Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
 Return your list ordered alphabetically by email starting with A. */
@@ -104,8 +100,6 @@ ORDER BY miliseconds DESC;
 
 
 
-
-/* Question Set 3 - Advance */
 
 /* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
 
